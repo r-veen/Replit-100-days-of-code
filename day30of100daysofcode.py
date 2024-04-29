24037119cbd7ee0e4c83e199a138f45533bec287
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-04-29 12:25:00 +0200

    day30

--- day30of100daysofcode.py
+++ day30of100daysofcode.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("day 30 of 100 days of code replit")
+
+print("30 Days Down - What did you think?")
+print()
+learnt_today = []
+for day in range(1, 31):
+    i = input(f"What did you think about day: {day} ")
+    learnt_today.append(i)
+
+print(f"day: {day: ^2}{learnt_today: ^2}." end="\n")
