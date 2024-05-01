c42f489145e50c550ddfe9ca013dd1dc2136718a
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-05-01 14:19:57 +0200

    The start of  the first UI

--- Day-31/day-31.py
+++ Day-31/day-31.py
@@ -1,1 +1,19 @@
-print("hello, world")
+
+def newPrint(colour, word):
+    if colour =="red":
+        print("\033[31m",word, sep="", end="")
+    elif colour =="yellow":
+        print("\033[33m",word,sep="", end="")
+    elif colour =="green":
+        print("\033[32m",word,sep="", end="")
+    elif colour =="blue":
+        print("\033[34m",word,sep="", end="")
+    elif colour == "white":
+        print("\033[0m", word, sep="", end="")
+newPrint("red", "=")
+newPrint("white", "=")
+newPrint("blue", "=")
+newPrint("yellow", "Music App")
+newPrint("red", "=")
+newPrint("white", "=")
+newPrint("blue", "=")
