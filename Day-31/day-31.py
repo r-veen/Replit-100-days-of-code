ff4b7b76de1219153597dba164bd952e31f62725
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-05-01 14:48:51 +0200

    adding more

--- Day-31/day-31.py
+++ Day-31/day-31.py
@@ -10,10 +10,19 @@ def newPrint(colour, word):
         print("\033[34m",word,sep="", end="")
     elif colour == "white":
         print("\033[0m", word, sep="", end="")
+text = "="
+
 newPrint("red", "=")
 newPrint("white", "=")
 newPrint("blue", "=")
-newPrint("yellow", "Music App")
+music = "Music App"
+newPrint("yellow", f"{music: >15}")
 newPrint("red", "=")
 newPrint("white", "=")
 newPrint("blue", "=")
+print()
+print()
+radio = "Radio Gaga"
+print(f"{radio: >15}")
+queen = "Queen"
+print(f"{queen: >10}")
