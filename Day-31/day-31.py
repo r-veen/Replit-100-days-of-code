9ef18065d3afcdb4a4c17289683807f33f516303
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-05-01 19:15:32 +0200

    first version

--- Day-31/day-31.py
+++ Day-31/day-31.py
@@ -1,4 +1,3 @@
-
 def newPrint(colour, word):
     if colour =="red":
         print("\033[31m",word, sep="", end="")
@@ -10,25 +9,51 @@ def newPrint(colour, word):
         print("\033[34m",word,sep="", end="")
     elif colour == "white":
         print("\033[0m", word, sep="", end="")
-text = "="
-
-newPrint("red", "=")
+text = '='
+newPrint("red", f"{text: >15}")
 newPrint("white", "=")
 newPrint("blue", "=")
-music = "Music App"
-newPrint("yellow", f"{music: >15}")
+newPrint("yellow", "Music App")
 newPrint("red", "=")
 newPrint("white", "=")
 newPrint("blue", "=")
 print()
-radio = "\033[0mRadio Gaga"
-print(f"{radio: >20}")
-queen = "\033[33mQueen"
-print(f"{queen: >16}")
+radio = "Radio Gaga"
+newPrint("white",f"{radio: >20}")
+newPrint("yellow","")
+print()
+queen = "Queen"
+print(f"{queen: >15}")
 print()
 newPrint("white", "Prev")
 print()
-next = "\033[32m""Next"
-print(f"{next: >15}")
+next = "Next"
+newPrint("green",f"{next: >10}")
+print()
 pause = "\033[38;5;201mPause"
 print(f"{pause: >28}")
+print()
+print()
+title = "Welcome to"
+newPrint("white",f"{title: >20}")
+print()
+line = "--"
+newPrint("blue",f"{line: >5}")
+title2 = "ARMBOOK"
+print(f"{title2: >10}",end="")
+print(f"{line: >5}")
+print()
+text1 = "Definitely not a rip off of a certain other social networking site."
+newPrint("yellow",f"{text1: >70}")
+print()
+print()
+print()
+text3 = "Honest."
+newPrint("red",f"{text3: >10}")
+name = "Username:"
+password = "Password:"
+print()
+print()
+newPrint("white",f"{name: >12}")
+print()
+newPrint("white",f"{password: >12}")
