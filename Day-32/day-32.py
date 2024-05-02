7718e2ea496a84a5899f07185ddee1a786002555
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-05-02 18:22:11 +0200

    finish

--- Day-32/day-32.py
+++ Day-32/day-32.py
@@ -1,3 +1,4 @@
-list = ["bro", "hi", "no", "nah"]
-for i in list:
-    print(i) 
+import random
+language = ["Hello there!", "Konnichiwa!", "Guten Tag!", "Bore Da!", "Bonjour", "Bom dia"]
+number = random.randint(0,len(language))
+print(f"{language[number]}")
