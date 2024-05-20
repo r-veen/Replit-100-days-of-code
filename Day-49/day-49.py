8a5956ec023a5f9b8fc9bc7baa3f7e44a02157e4
Rouven vanderveen <rouvenvanderveen@gmail.com>
2024-05-20 12:04:42 +0200

    day-49

--- Day-49/day-49.py
+++ Day-49/day-49.py
@@ -1,5 +1,10 @@
 import os, time
 
+def prettyprint(lines):
+    for line in lines:
+        print(line.strip())
+        time.sleep(1)
+
 f = open("high.score", "a+")
 while True:
     initial = input("Input your initials > ").strip()
@@ -8,12 +13,32 @@ while True:
     score = int(input("Input your score > "))
     while score > 100000 or score < 1:
         score = int(input("It needs to be a positive number and under 100000"))
-    f.write(f"Initial: {initial}\nScore:{score}\n")
+    f.write(f"Initial: {initial}\nScore: {score}\n")
     exit = input("Exit? > ").strip().lower()
     if exit == "yes":
         break
     time.sleep(2)
     os.system("clear")
-p = f.read()
-print(p)
 f.close()
+
+f = open("high.score", "r")
+lines = f.readlines()
+f.close()
+
+if len(lines) % 2 != 0:
+    print("Die Anzahl der Zeilen ist ungerade. Bitte überprüfe die Datei.")
+    exit()
+
+highest_score = 0
+winner_initial = ""
+
+for i in range(0, len(lines), 2):
+    initial = lines[i].split(":")[1].strip()
+    score = int(lines[i+1].split(":")[1].strip())
+    
+    if score > highest_score:
+        highest_score = score
+        winner_initial = initial
+
+print(f"Winner: {winner_initial} with a score of {highest_score}")
+prettyprint(lines)
