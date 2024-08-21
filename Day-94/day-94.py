919a403d2f1eccdb36a4ace445ba60714c92f6b2
Rouven <140442950+r-veen@users.noreply.github.com>
2024-08-21 11:45:58 +0200

    broken days

--- Day-94/day-94.py
+++ Day-94/day-94.py
@@ -1,2 +1,2 @@
-#I can't do this tag because this function of ChatGPT no longer exists.
+#I can't do this day because this function of ChatGPT no longer exists.
 #I tried everything, but I couldn't succeed, and there is no solution on Replit either.
---
+++ Day-95/day-95.py
@@ -0,0 +1,2 @@
+#I can't do this day because this function of ChatGPT no longer exists.
+#I tried everything, but the solution in Replit doesn't work either.
